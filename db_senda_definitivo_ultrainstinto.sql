d70bdafd0a39869c28dc50573c09ad867cfae679
paudpr <paula.p.ramirez@gmail.com>
2023-11-16 15:34:43 +0100

    change db

--- db_senda_definitivo_ultrainstinto.sql
+++ db_senda_definitivo_ultrainstinto.sql
@@ -142,34 +142,6 @@ INSERT INTO `booking_vuelo` (`id`, `vu_nombre`, `vu_origen`, `vu_destino`, `vu_p
 (11, 'Barcelona - Madrid', 'Barcelona', 'Madrid', 164.00, '8H', 'vuelo/building-470925_640.jpg');
 
 --
--- Volcado de datos para la tabla `django_migrations`
---
-
-INSERT INTO `django_migrations` (`id`, `app`, `name`, `applied`) VALUES
-(1, 'contenttypes', '0001_initial', '2023-11-16 11:44:14.032625'),
-(2, 'contenttypes', '0002_remove_content_type_name', '2023-11-16 11:44:14.069788'),
-(3, 'auth', '0001_initial', '2023-11-16 11:44:14.237061'),
-(4, 'auth', '0002_alter_permission_name_max_length', '2023-11-16 11:44:14.275712'),
-(5, 'auth', '0003_alter_user_email_max_length', '2023-11-16 11:44:14.280754'),
-(6, 'auth', '0004_alter_user_username_opts', '2023-11-16 11:44:14.285782'),
-(7, 'auth', '0005_alter_user_last_login_null', '2023-11-16 11:44:14.289341'),
-(8, 'auth', '0006_require_contenttypes_0002', '2023-11-16 11:44:14.292343'),
-(9, 'auth', '0007_alter_validators_add_error_messages', '2023-11-16 11:44:14.296347'),
-(10, 'auth', '0008_alter_user_username_max_length', '2023-11-16 11:44:14.300874'),
-(11, 'auth', '0009_alter_user_last_name_max_length', '2023-11-16 11:44:14.304878'),
-(12, 'auth', '0010_alter_group_name_max_length', '2023-11-16 11:44:14.314410'),
-(13, 'auth', '0011_update_proxy_permissions', '2023-11-16 11:44:14.318944'),
-(14, 'auth', '0012_alter_user_first_name_max_length', '2023-11-16 11:44:14.322956'),
-(15, 'user', '0001_initial', '2023-11-16 11:44:15.007668'),
-(16, 'admin', '0001_initial', '2023-11-16 11:44:15.102079'),
-(17, 'admin', '0002_logentry_remove_auto_add', '2023-11-16 11:44:15.109605'),
-(18, 'admin', '0003_logentry_add_action_flag_choices', '2023-11-16 11:44:15.115611'),
-(19, 'booking', '0001_initial', '2023-11-16 11:44:15.253647'),
-(20, 'booking', '0002_initial', '2023-11-16 11:44:15.861470'),
-(21, 'sessions', '0001_initial', '2023-11-16 11:44:15.882094'),
-(22, 'booking', '0003_alter_reserva_res_precio', '2023-11-16 12:23:15.532301');
-
---
 -- Volcado de datos para la tabla `user_cliente`
 --
 
